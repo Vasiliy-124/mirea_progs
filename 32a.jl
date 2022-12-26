f8589392941e899b2e216dbfeaff6bce2a189e27
Vasiliy-124 <79218550+Vasiliy-124@users.noreply.github.com>
2022-12-26 14:41:10 +0300

    Delete 32a.jl

--- 32a.jl
+++
@@ -1,8 +0,0 @@
-include("somefunctions.jl")
-
-function show_typetree(type::DataType, level=0)
-    println(" " ^ level, type)
-    for t in subtypes(type) 
-        show_typetree(t, level+3) 
-    end
-end
